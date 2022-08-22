fec434f5049019ddcd549e3ede6008dcffe786a3
0xMirim <95051992+0xMirim@users.noreply.github.com>
2022-08-22 16:13:49 +0400

    refactor: Remove remaining eslint-disable comments in site files
    Removed all eslint-disable comments, aside from one in next.config.js

--- pages/api/ape-vaults.tsx
+++ pages/api/ape-vaults.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/prefer-for-of */
 import	{Contract}	from	'ethcall';
 import 	{providers} from '@yearn-finance/web-lib/utils';
 import	{toAddress}				from	'utils';
@@ -23,35 +22,37 @@ export function getProvider(chain = 1): ethers.providers.JsonRpcProvider {
 	return new ethers.providers.InfuraProvider('homestead', '9aa3d95b3bc440fa88ea12eaa4456161');
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-async function fetchStrategies({vaultAddress, network}: {vaultAddress: string, network: number}) {
+async function fetchStrategies({vaultAddress, network}: {vaultAddress: string, network: number}): Promise<string[]> {
 	const	vaultContract = new Contract(
 		vaultAddress,
 		[{'stateMutability': 'view', 'type': 'function', 'name': 'withdrawalQueue', 'inputs': [{'name': 'arg0', 'type': 'uint256'}], 'outputs': [{'name': '', 'type': 'address'}], 'gas': '4057'}]
 	);
 	const	strategiesIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 	const 	calls = [];
-	for (let i = 0; i < strategiesIndex.length; i++) {
-		calls.push(vaultContract.withdrawalQueue(strategiesIndex[i]));
+	for (const strategyIndex of strategiesIndex) {
+		calls.push(vaultContract.withdrawalQueue(strategyIndex));
 	}
 	const	ethcallProvider = await providers.newEthCallProvider(getProvider(network));
 	const	callResult = await ethcallProvider.tryAll(calls);
-	return	callResult.filter((a): boolean => a !== ethers.constants.AddressZero);
+	const addresses = callResult.filter((a): boolean => a !== ethers.constants.AddressZero) as string[];
+
+	return	addresses;
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-async function fetchNames({addresses, network}: {addresses: string[], network: number}) {
+async function fetchNames({addresses, network}: {addresses: string[], network: number}): Promise<string[]> {
 	const 	calls = [];
-	for (let index = 0; index < addresses.length; index++) {
+	for (const address of addresses) {
 		const	strategyContract = new Contract(
-			addresses[index],
+			address,
 			[{'inputs':[], 'name':'name', 'outputs':[{'internalType':'string', 'name':'', 'type':'string'}], 'stateMutability':'view', 'type':'function'}]
 		);
 		calls.push(strategyContract.name());
 	}
 	const	ethcallProvider = await providers.newEthCallProvider(getProvider(network));
 	const	callResult = await ethcallProvider.tryAll(calls);
-	return	callResult.filter((a): boolean => a !== ethers.constants.AddressZero);
+	const strategyNames = callResult.filter((a): boolean => a !== ethers.constants.AddressZero) as string[];
+
+	return strategyNames;
 }
 
 
@@ -60,8 +61,8 @@ async function getVaultStrategies({vaultAddress, network, stratTree}: {vaultAddr
 
 	const 	strategies = [];
 	let		hasMissingStrategiesDescriptions = false;
-	for (let i = 0; i < vaultStrategies.length; i++) {
-		const	strategyAddress = toAddress(vaultStrategies[i] as string);
+	for (const strategy of vaultStrategies) {
+		const	strategyAddress = toAddress(strategy);
 		const	details = stratTree[strategyAddress];
 		if (details) {
 			if (!details?.description) {
@@ -84,8 +85,7 @@ async function getVaultStrategies({vaultAddress, network, stratTree}: {vaultAddr
 		}
 	}
 	const	strategyWithNoName = [];
-	for (let index = 0; index < strategies.length; index++) {
-		const strategy = strategies[index];
+	for (const strategy of strategies) {
 		if (!strategy.name) {
 			strategyWithNoName.push(strategy.address);
 		}
@@ -93,8 +93,7 @@ async function getVaultStrategies({vaultAddress, network, stratTree}: {vaultAddr
 	const	noNameNames = await fetchNames({addresses: strategyWithNoName, network});
 	
 	let		noNameIndex = 0;
-	for (let index = 0; index < strategies.length; index++) {
-		const strategy = strategies[index];
+	for (const strategy of strategies) {
 		if (!strategy.name) {
 			strategy.noIPFS = true;
 			strategy.name = noNameNames[noNameIndex++] as string;
@@ -108,10 +107,8 @@ async function getStrategies({network}: {network: number}): Promise<TVaultWithSt
 	const	allStrategiesAddr: TStrategyMetadata[] = await (await fetch(`${process.env.META_API_URL}/${network}/strategies/all`)).json();
 	const	stratTree: TStratTree = {};
 
-	for (let index = 0; index < allStrategiesAddr.length; index++) {
-		const stratDetails = allStrategiesAddr[index];
-		for (let jindex = 0; jindex < (stratDetails.addresses).length; jindex++) {
-			const address = stratDetails.addresses[jindex];
+	for (const stratDetails of allStrategiesAddr) {
+		for (const address of stratDetails.addresses) {
 			stratTree[toAddress(address)] = {
 				description: stratDetails.description,
 				name: stratDetails.name
@@ -123,8 +120,7 @@ async function getStrategies({network}: {network: number}): Promise<TVaultWithSt
 	const	vaultsWithStrats = [];
 	const	filteredVaults = vaults.filter((e): boolean => e.status !== 'endorsed');
 
-	for (let index = 0; index < filteredVaults.length; index++) {
-		const vault: TApeVault = filteredVaults[index];
+	for (const vault of filteredVaults) {
 		const	[strategies, hasMissingStrategiesDescriptions] = await getVaultStrategies({
 			vaultAddress: vault.address,
 			network,
@@ -147,8 +143,8 @@ async function getStrategies({network}: {network: number}): Promise<TVaultWithSt
 
 const	vaultsMapping: {[key: number]: TVaultWithStrats[] } = {};
 const		vaultsMappingAccess: {[key: number]: number} = {};
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export default async function handler(req: NextApiRequest, res: NextApiResponse){
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
 	const		{revalidate} = req.query;
 	const network = Number(req.query.network);
 
